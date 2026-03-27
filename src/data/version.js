092913d4ec51c1519f8f072681ddfdf9b61050bd
Whynchu <samuelboxold@gmail.com>
2026-03-27 19:47:44 -0400

    Polish mobile menu spacing

--- src/data/version.js
+++ src/data/version.js
@@ -1,4 +1,4 @@
-const VERSION = { num: '0.68', label: 'Menu Boards' };
+const VERSION = { num: '0.69', label: 'Menu Polish' };
 
 function formatVersionTag(version = VERSION) {
   return `// prototype v${version.num} - ${version.label}`;
